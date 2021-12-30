a197347f6e315b25bfaa4aa207c5e7f583392b57
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2021-12-30 15:56:56 +0200

    fix link

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,72 +1,69 @@
-import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {Store} from '@ngrx/store';
-import {FormControl, Validators} from '@angular/forms';
-import {requestSignInWithGoogleAction, requestSignInAction} from '../../auth/auth.actions';
-import {MyErrorStateMatcher} from '../../models/class/errorStateMather';
-import {AnimationStates} from '../../models/enum/animationStates';
-import {animationFadeAndScale} from '../../models/animations/animationFadeAndScale'
-
-
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { FormControl, Validators } from '@angular/forms';
+import {
+    requestSignInWithGoogleAction,
+    requestSignInAction,
+} from '../../auth/auth.actions';
+import { MyErrorStateMatcher } from '../../models/class/errorStateMather';
+import { AnimationStates } from '../../models/enum/animationStates';
+import { animationFadeAndScale } from '../../models/animations/animationFadeAndScale';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
-  animations: [animationFadeAndScale]
+    selector: 'app-sign-in',
+    templateUrl: './sign-in.component.html',
+    styleUrls: ['./sign-in.component.scss'],
+    animations: [animationFadeAndScale],
 })
 export class SignInComponent implements OnInit, OnDestroy, AfterViewInit {
-  panelOpenState = false;
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  passwordFormControl = new FormControl('', [
-    Validators.required,
-  ])
+    panelOpenState = false;
+    emailFormControl = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+    passwordFormControl = new FormControl('', [Validators.required]);
 
-  emailMatcher = new MyErrorStateMatcher();
-  passMatcher = new MyErrorStateMatcher();
+    emailMatcher = new MyErrorStateMatcher();
+    passMatcher = new MyErrorStateMatcher();
 
-  animationState = 'initial'
+    animationState = 'initial';
 
-  constructor(
-    private store: Store,
-    private router: Router
-  ) {}
+    constructor(private store: Store, private router: Router) {}
 
-  ngOnInit(): void {}
+    ngOnInit(): void {}
 
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.animationState = AnimationStates.Loaded;
-    })
-  }
+    ngAfterViewInit(): void {
+        setTimeout(() => {
+            this.animationState = AnimationStates.Loaded;
+        });
+    }
 
-  ngOnDestroy(): void {}
+    ngOnDestroy(): void {}
 
-  loginWithGoogle() {
-    this.store.dispatch(requestSignInWithGoogleAction())
-  }
+    loginWithGoogle() {
+        this.store.dispatch(requestSignInWithGoogleAction());
+    }
 
-  loginWithFacebook() {}
+    loginWithFacebook() {}
 
-  loginWithEmail() {
-    this.store.dispatch(requestSignInAction({
-      username: this.emailFormControl.value,
-      password: this.passwordFormControl.value
-    }))
-  }
+    loginWithEmail() {
+        this.store.dispatch(
+            requestSignInAction({
+                username: this.emailFormControl.value,
+                password: this.passwordFormControl.value,
+            })
+        );
+    }
 
-  isDisabled(): boolean {
-    return !this.emailFormControl.valid || !this.passwordFormControl.valid;
-  }
+    isDisabled(): boolean {
+        return !this.emailFormControl.valid || !this.passwordFormControl.valid;
+    }
 
-  routeTo(url: string) {
-    this.animationState = AnimationStates.Destroyed;
-    setTimeout(() => {
-      this.router.navigate([url])
-    }, 300)
-  }
-
+    routeTo(url: string) {
+        this.animationState = AnimationStates.Destroyed;
+        setTimeout(() => {
+            this.router.navigate([url]);
+        }, 300);
+    }
 }
