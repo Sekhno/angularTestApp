324f799724beed497cdd0e935d1e1df7e93f5ff4
Dmytro Sekhno <dmytro.sekhno@semantrum.net>
2022-01-04 17:13:04 +0200

    update animation

--- src/app/models/animations/animationFadeAndScale.ts
+++ src/app/models/animations/animationFadeAndScale.ts
@@ -1,5 +1,9 @@
 import {trigger, style, transition, animate, keyframes} from '@angular/animations';
 
+const TRANSITION = '{{duration}}ms ease-in-out';
+const DURATION = {params: {duration: 300}};
+const STAGGER = 300;
+
 export const animationFadeAndScale = trigger('animationFadeAndScale', [
   transition('initial => loaded', [
     // style({ opacity: 0, transform: 'scale(0.8)' }),
@@ -12,4 +16,17 @@ export const animationFadeAndScale = trigger('animationFadeAndScale', [
   transition('loaded => destroyed', [
     animate('.3s', style({ opacity: 0, transform: 'scale(0.6)' })),
   ]),
-])
+]);
+
+export const heightCollapse = trigger('tuiHeightCollapse', [
+  transition(
+      ':enter',
+      [style({height: 0}), animate(TRANSITION, style({height: '*'}))],
+      DURATION,
+  ),
+  transition(
+      ':leave',
+      [style({height: '*'}), animate(TRANSITION, style({height: 0}))],
+      DURATION,
+  ),
+]);
